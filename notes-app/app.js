bc981758682f1393a24dd21dc2fe4755b64c793a
WebApp07 <aminezouguig@proton.me>
2025-01-06 20:57:10 +0100

    Importing Your Own Files

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,3 @@
-const fileSystem = require("fs");
-
-fileSystem.writeFileSync("notes.txt", "My name is Amine.!");
-fileSystem.appendFileSync("notes.txt", " We should learn Cyber Security.");
+const getNotes = require("./notes.js");
+const notes = getNotes();
+console.log(notes);
---
+++ notes-app/utils.js
@@ -0,0 +1,9 @@
+console.log("File from utils.js");
+
+const name = "Zou";
+
+const add = function (a, b) {
+  return a + b;
+};
+
+module.exports = add;
