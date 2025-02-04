4228ee0e67e4c2325350eff6bce96256d8062589
WebApp07 <aminezouguig@proton.me>
2025-02-04 00:58:00 -0800

    Getting Input from Users

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,10 @@
 const chalk = require("chalk");
 const getNotes = require("./notes.js");
 
-const notes = getNotes();
+const command = process.argv[2];
 
-console.log(notes);
-
-console.log(chalk.red.inverse.bold("Error!"));
+if (command === "add") {
+  console.log("Adding note!");
+} else if (command === "remove") {
+  console.log("Removing note!");
+}
